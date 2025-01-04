2ce8b27533a138b3a656d9e6ddb196926cecbf37
hanxuedong <hanxd3011@gmail.com>
2025-01-04 15:59:55 +0800

    build: 更新资源路径和 GitHub 仓库名
    - 修改 index.html 中的 favicon 和 main.js 路径，使用相对路径代替绝对路径
    - 更新 vite.config.js 中的 base 配置，设置为新的 GitHub 仓库名 '/wscrcpy/'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue3-github-pages-demo/', // 设置基本公共路径
+  base: '/wscrcpy/', // 设置为您的 GitHub 仓库名
 })
